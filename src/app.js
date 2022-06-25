8ef8542e31b4e53027d94e67b7a69b33e1640ec5
Abel Tran <abeltran.develop@gmail.com>
2022-06-26 00:12:26 +0700

    Update cors methods

--- src/app.js
+++ src/app.js
@@ -18,10 +18,12 @@ app.use(
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Accept, Content-Type, Authorization, X-Requested-With"
   );
+
   next();
 });
 
