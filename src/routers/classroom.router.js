62d8aed9fc9efc39a6fb6f9924c6286e9e4f5ea1
Abel Tran <abeltran.develop@gmail.com>
2022-06-26 00:21:07 +0700

    Update header

--- src/routers/classroom.router.js
+++ src/routers/classroom.router.js
@@ -1,17 +1,31 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/import', require('../handlers/classrooms/import-classrooms'));
-router.get('/', require('../handlers/classrooms/get-all-classrooms'));
-router.get('/:classroomId', require('../handlers/classrooms/get-classroom'));
+router.use(cors());
 
-router.post('/:classroomId/students/import', require('../handlers/students/import-students'));
-router.get('/:classroomId/students', require('../handlers/students/get-all-students-in-classroms'));
-router.get('/:classroomId/students/:studentId', require('../handlers/students/get-student'));
+router.post("/import", require("../handlers/classrooms/import-classrooms"));
+router.get("/", require("../handlers/classrooms/get-all-classrooms"));
+router.get("/:classroomId", require("../handlers/classrooms/get-classroom"));
 
-router.post('/:classroomId/students/:studentId/vote', require('../handlers/histories/vote-student'));
+router.post(
+  "/:classroomId/students/import",
+  require("../handlers/students/import-students")
+);
+router.get(
+  "/:classroomId/students",
+  require("../handlers/students/get-all-students-in-classroms")
+);
+router.get(
+  "/:classroomId/students/:studentId",
+  require("../handlers/students/get-student")
+);
 
-router.get('/', (req, res) => {
-  res.send('Classroom');
+router.post(
+  "/:classroomId/students/:studentId/vote",
+  require("../handlers/histories/vote-student")
+);
+
+router.get("/", (req, res) => {
+  res.send("Classroom");
 });
 
 module.exports = router;
